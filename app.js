954456a5da2e8c592b437e46c20251a43b75fe84
Pravin <avacharpravin123@gmail.com>
2025-09-15 20:47:21 +0530

    added js a bit

---
+++ app.js
@@ -0,0 +1,43 @@
+let tasks = [];
+
+const addTask = () => {
+  const taskInput = document.getElementById("taskInput");
+  const text = taskInput.value.trim();
+
+  if (text) {
+    tasks.push({ text: taskInput, completed: false });
+
+    updateTaskList();
+  }
+};
+
+const updateTaskList = () => {
+  const taskList = document.getElementById("task-list");
+  taskList.innerHTML = "";
+
+  tasks.forEach((task, index) => {
+    const listItem = document.createElement("li");
+
+    listItem.innerHTML = `
+            <div class="taskItem">
+                <div class="task ${task.completed ? "completed" : ""}">
+                    <input type="checkbox" class=checkbox" ${
+                      task.completed ? "checked" : ""
+                    }/>
+                    <p>${task.text}</p>
+                </div>
+                <div class="icons">
+                    <img src="./img/edit.png" onClick="editTask(${index}"/>
+                    <img src="./img/bin.png" onClick="deleteTask(${index}/>
+                </div>
+            </div>
+        `;
+    listItem.addEventListener("change", () => toggleTaskComplete());
+    taskList.append(listItem);
+  });
+};
+
+document.getElementById("newTask").addEventListener("click", function (e) {
+  e.preventDefault();
+  addTask();
+});
